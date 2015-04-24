e5cee6e1e67677dc8d6987433790138fa5c78a8d
Yoshiyuki Koyanagi <moutend@gmail.com>
2015-04-24 22:05:24 +0900

    Add

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo sed -i 's/PermitRootLogin without-password/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config
+sudo sed -i 's/session\s*required\s*pam_loginuid.so/session optional pam_loginuid.so/g' /etc/pam.d/sshd
+sudo echo 'PasswordAuthentication no' | sudo tea -a /etc/ssh/sshd_config
