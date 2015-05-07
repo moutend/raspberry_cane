109cd14e52b778d76f228fb82dd3a9d0d7c7f8d7
Yoshiyuki Koyanagi <moutend@gmail.com>
2015-05-07 12:02:22 +0900

    Update

--- distance.py
+++ distance.py
@@ -1,56 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time, os, sys, datetime
+import time, datetime, os, sys, signal
 import RPi.GPIO as GPIO
 
-sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+TRIG = 11
+ECHO = 13
 
-MAX_OFF = 200
-MAX_ON  = 5000
-TRIG    = 11
-ECHO    = 13
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
 
-def reading():
-  GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BOARD)
+def cleanup_GPIO(signal, frame):
+  GPIO.output(TRIG, GPIO.LOW)
+  GPIO.output(TRIG, False)
+  GPIO.cleanup()
+  sys.exit(0)
 
-  GPIO.setup(TRIG,GPIO.OUT)
-  GPIO.setup(ECHO,GPIO.IN)
-
+def measure():
   GPIO.output(TRIG, GPIO.LOW)
   GPIO.output(TRIG, True)
   time.sleep(0.00001)
   GPIO.output(TRIG, False)
 
+  limit_signal_off = 300
+  limit_signal_on  = 5000
   signal_off = 0
-  k = 0
-  while GPIO.input(ECHO) == 0:
-    signal_off = time.time()
-    k += 1
-    if k > MAX_OFF:
-      signal_off = 0
-      break
-
-  if signal_off == 0:
-    return -1
-
   signal_on  = 0
-  k = 0
-  while GPIO.input(ECHO) == 1:
-    signal_on = time.time()
-    k += 1
-    if k > MAX_ON:
-      signal_on = 0
-      break
 
-  if signal_on == 0:
-    return -1
+  while not GPIO.input(ECHO):
+    signal_off = time.time()
+    limit_signal_off -= 1
+    if limit_signal_off == 0: return -1
+
+  while GPIO.input(ECHO):
+    signal_on = time.time()
+    limit_signal_on -= 1
+    if limit_signal_on == 0: return -1
 
   distance = (signal_on - signal_off) * 17000
-  GPIO.cleanup()
-  return (signal_on - signal_off) * 17000
+  if distance <= 400.0:
+    return distance
+  else:
+    return -1
+
+signal.signal(signal.SIGINT, cleanup_GPIO)
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
 while True:
-  print datetime.datetime.now(), reading()
+  print datetime.datetime.now(), measure()
   time.sleep(0.05)
