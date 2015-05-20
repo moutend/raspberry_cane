076c47662ca7bbf2de2c033c76e3e4b073786d8a
Yoshiyuki Koyanagi <moutend@gmail.com>
2015-05-20 14:10:18 +0900

    Update

--- key.py
+++ key.py
@@ -5,14 +5,20 @@ import os
 import sys
 import signal
 import time
+import datetime
+import threading
 import RPi.GPIO as GPIO
 from multiprocessing import Process
 
 PULSE = 0.05
 VIBE = 12
 STEP = 0.05
+__interval = 1.0
+__timer = None
 
 def cleanup_GPIO(signal, frame):
+  global __timer
+  __timer.cancel()
   print "\nDO:   cleanup_GPIO()"
   GPIO.output(VIBE, GPIO.LOW)
   GPIO.output(VIBE, False)
@@ -33,23 +39,26 @@ def getKey():
    termios.tcsetattr(0, termios.TCSANOW, old_settings)
   return ord(ch)
 
-def vibrate(wait, interval):
-  GPIO.output(VIBE, GPIO.LOW)
+def shake():
+  GPIO.output(VIBE, True)
+  time.sleep(PULSE)
   GPIO.output(VIBE, False)
+  return time.time()
 
-  time.sleep(wait)
-  while True:
-    GPIO.output(VIBE, True)
-    time.sleep(PULSE)
-    GPIO.output(VIBE, False)
-    time.sleep(interval)
+def kick():
+  global __timer
+  global __interval
+
+  __timer = threading.Timer(__interval, kick)
+  __timer.start()
+  shake()
 
 if __name__ == '__main__':
   print "To control, hit the following key."
   print "=================================="
   print "Ctrl-C => Quit"
   print "k      => Slow down"
-  print "j      => Speed up"
+  print "j      => Speed up\n"
 
   signal.signal(signal.SIGINT, cleanup_GPIO)
   sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
@@ -58,28 +67,14 @@ if __name__ == '__main__':
   GPIO.setmode(GPIO.BOARD)
   GPIO.setup(VIBE, GPIO.OUT)
 
-  interval = 1.0 - PULSE
-
-  p = Process(target=vibrate, args=(0, interval,))
-  p.daemon = True
-  p.start()
-  pid = p.pid
-
+  kick()
   while True:
     c = getKey()
 
-    if c == 106 and interval >= 0.1:
-      interval -= STEP
-    elif c == 107 and interval < 1.0 - PULSE:
-      interval += STEP
-    else:
-      continue
+    if c == 106 and __interval > 0.1:
+      __interval -= 0.05
 
-    os.kill(pid, signal.SIGTERM)
+    if c == 107 and __interval < 1.0:
+      __interval += 0.05
 
-    p = Process(target=vibrate, args=(0, interval,))
-    p.daemon = True
-    p.start()
-    pid = p.pid
-
-    print "\ninterval: %s" % interval
+    print "interval: %s" % __interval
